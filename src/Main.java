e07025c7f9771be01f1e1e27afa06c59593bcfca
Анастасия Антонова <96870104+AntonovaAnastasiya@users.noreply.github.com>
2022-02-22 23:36:51 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         BmiService service = new BmiService();
-        float weight = 53.3f;
-        float bmi = service.calculate(weight);
-        
-        float height = 1f;
-        if (height > 0) {
-            System.out.println("Индекс массы тела " + bmi);
-        }
+
+        float result = service.calculate(53.3f, 1.6f);
+        System.out.println("Индекс массы тела " + result);
     }
 }
